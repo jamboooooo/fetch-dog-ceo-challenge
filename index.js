d65af5a7769100612a7047b300a4b5d3c96bcadd
jamboooooo <hisllam35@gmail.com>
2021-06-01 14:38:55 +0300

    commit

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // количество загружаемых изображений
-  const IMAGES_COUNT = 4;
+    // количество загружаемых изображений
+    const IMAGES_COUNT = 4;
 
-  // ссылка для загрузки изображений
-  const IMAGES_URL = `https://dog.ceo/api/breeds/image/random/${IMAGES_COUNT}`;
+    // ссылка для загрузки изображений
+    const IMAGES_URL = `https://dog.ceo/api/breeds/image/random/${IMAGES_COUNT}`;
 
-  // ссылка для загрузки списка пород
-  const BREEDS_URL = "https://dog.ceo/api/breeds/list";
+    // ссылка для загрузки списка пород
+    const BREEDS_URL = "https://dog.ceo/api/breeds/list";
 
-  // узел, в котором будет список изображений
-  const imagesContainer = document.querySelector('.images');
+    // узел, в котором будет список изображений
+    const imagesContainer = document.querySelector('.images');
 
-  // узел, в котором будет список пород
-  const breedsContainer = document.querySelector('.breeds');
+    // узел, в котором будет список пород
+    const breedsContainer = document.querySelector('.breeds');
 
-  // узел кнопки обновления
-  const button = document.querySelector('button');
+    // узел кнопки обновления
+    const button = document.querySelector('button');
 
-  // сразу загружаем изображения
-  fetchAndRenderImages();
+    // загружаем список пород
+    const fetchBreedsList = () => {
+        const data = fetch(BREEDS_URL)
+            .then(res => res.json())
+            .then(breeds => {
+                breeds.message.forEach(item => {
+                    const li = document.createElement('li')
+                    li.textContent = item
+                    breedsContainer.append(li)
+                })
+            })
+    }
+    fetchBreedsList();
 
-  // загружаем список пород
-  fetchBreedsList();
+    // еще раз загружаем изображения, если кликнули на кнопку обновления
+    button.addEventListener('click', () => {
+        fetchAndRenderImages();
+    });
 
-  // еще раз загружаем изображения, если кликнули на кнопку обновления
-  button.addEventListener('click', () => {
-    fetchAndRenderImages();
-  });
+    // ТВОЙ КОД
+    // сразу загружаем изображения
 
-  // ТВОЙ КОД
-
-});
+    const fetchAndRenderImages = () => {
+        const data = fetch(IMAGES_URL)
+            .then(res => res.json())
+            .then(imgs => {
+                imgs.message.forEach(item => {
+                    const img = document.createElement('img')
+                    img.src = item;
+                    img.style.width = '250px'
+                    img.style.height = '250px'
+                    imagesContainer.append(img)
+                });
+            })
+    }
+    fetchAndRenderImages()
+})
